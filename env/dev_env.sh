4f21d0ab4838327563835da3c1ffc54b6c19653b
Sasha Keskin <skeskin@bwi.earth>
2025-02-22 14:11:29 +0100

    wtf

--- env/dev_env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export ws_address="0.0.0.0:1337"
-export flask_address="0.0.0.0:8000"
--- env/local_env.sh
+++ env/local_env.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-export ws_address="35.242.159.190:1337"
-export flask_address="35.242.159.190:8000"
+export GOOGLE_APPLICATION_CREDENTIALS="$HOME/.config/gcloud/legacy_credentials/spa.detente.france@gmail.com/adc.json"
+export GCLOUD_PROJECT="thalasshome-api"
+export flask_address="0.0.0.0:8000"

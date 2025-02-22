b522b687bc3e2fa7c43607f2ba46d27ba903908a
Sasha Keskin <skeskin@bwi.earth>
2025-02-22 12:22:37 +0100

    fixes

--- env/env.sh
+++ env/env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export GOOGLE_APPLICATION_CREDENTIALS="$HOME/.config/gcloud/legacy_credentials/spa.detente.france@gmail.com/adc.json"
-export GCLOUD_PROJECT="employees-dashboard-307021"
+export GCLOUD_PROJECT="thalasshome-api"
 

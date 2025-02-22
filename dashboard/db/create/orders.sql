45aef61b0bda3986f45676f38ea5a290c738423f
Sasha Keskin <skeskin@bwi.earth>
2025-02-22 20:27:30 +0100

    Define tables

---
+++ dashboard/db/create/orders.sql
@@ -0,0 +1,52 @@
+CREATE TYPE product_type AS ENUM ('jacuzzi2p', 'jacuzzi4p', 'other');
+
+CREATE TYPE order_status AS ENUM (
+  'ask', 'pending', 'assigned', 'in_delivery', 'delivered', 'canceled'
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    user_id      INT NOT NULL,
+    email        TEXT,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    gateway      TEXT,
+    total_price  NUMERIC,
+    title        TEXT,
+    shipping_address TEXT,
+    phone        TEXT,
+    status       order_status DEFAULT 'ask',
+    shipping_lat FLOAT, -- AVAILABLE IN THE RECEIVED HOOK :D
+    shipping_lon FLOAT,
+    delivery_man_id INTEGER,
+
+    CONSTRAINT fk_user
+      FOREIGN KEY (user_id)
+      REFERENCES users(id)
+);
+
+
+CREATE TABLE line_items (
+    id SERIAL PRIMARY KEY,
+    order_id   INT NOT NULL,
+    from_date DATE,
+    to_date DATE,
+    quantity   INT,
+    price      NUMERIC,
+    product    product_type NOT NULL,
+
+    CONSTRAINT fk_order
+      FOREIGN KEY (order_id)
+      REFERENCES orders(id)
+);
+
+
+CREATE TABLE delivery_stock (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    product product_type NOT NULL,
+    quantity INT NOT NULL DEFAULT 0,
+    CONSTRAINT fk_delivery_user
+      FOREIGN KEY (user_id)
+      REFERENCES users(id)
+);
--- dashboard/db/create/users.sql
+++ dashboard/db/create/users.sql
@@ -3,7 +3,12 @@ CREATE TABLE users (
     username TEXT NOT NULL,
     password TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
+    is_staff BOOLEAN DEFAULT FALSE,
     country TEXT,
     zone TEXT,
     phone_number TEXT
 );
+
+
+
+

7795b59330742fa93e2ab988a0c6a19c67628f33
Sasha Keskin <skeskin@bwi.earth>
2025-02-23 16:54:15 +0100

    Use postgis to check avail

--- dashboard/db/create/locations.sql
+++ dashboard/db/create/locations.sql
@@ -2,8 +2,7 @@ CREATE TABLE user_delivery_zones (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     zone_name TEXT NOT NULL,
-    center_lat FLOAT NOT NULL,
-    center_lon FLOAT NOT NULL,
+    center_geog geography(Point, 4326),
     radius_km FLOAT NOT NULL DEFAULT 30,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );

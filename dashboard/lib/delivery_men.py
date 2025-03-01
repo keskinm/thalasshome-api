93eface7e0db3f2b02240e1a56c5a5c5a80d4e6a
Sasha Keskin <skeskin@bwi.earth>
2025-03-01 13:57:20 +0100

    delivery men get orders

--- dashboard/lib/delivery_men.py
+++ dashboard/lib/delivery_men.py
@@ -2,7 +2,38 @@ from flask import request, session, Blueprint, jsonify
 
 from dashboard.constants import JACUZZI6P, JACUZZI4P
 from dashboard.db.client import supabase_cli
+from dashboard.lib.order.order import get_address, get_ship
+
 delivery_men_bp = Blueprint('delivery_men', __name__)
+
+
+@delivery_men_bp.route('/delivery_men/orders', methods=['GET'])
+def get_orders():
+    user_id = session["user_id"]
+
+    available_orders = supabase_cli.table("orders").select("*").is_("delivery_men_id", None).execute().data
+    ongoing_orders = supabase_cli.table("orders").select("*").eq("delivery_men_id", user_id).execute().data
+
+    def process_orders(orders):
+        results = []
+        for order in orders:
+            line_items = (supabase_cli.table("line_items").
+                          select("*").
+                          eq("order_id", order["id"]).
+                          execute().data)
+            ship, amount = get_ship(line_items)
+            results.append({
+                "address": get_address(order),
+                "phone": order["phone"],
+                "ship": ship,
+                "amount": amount
+            })
+        return results
+
+    return jsonify({
+        "available": process_orders(available_orders),
+        "ongoing": process_orders(ongoing_orders)
+    }), 200
 
 
 

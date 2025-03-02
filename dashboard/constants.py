f339637a0dfefbdb6ebb9a957509c6bbac6d42a9
Sasha Keskin <skeskin@bwi.earth>
2025-03-02 14:41:26 +0100

    Fix

--- dashboard/constants.py
+++ dashboard/constants.py
@@ -7,7 +7,7 @@ JACUZZI6P= 'jacuzzi6p'
 JACUZZI4P='jacuzzi4p'
 
 def normalize_jac_string(string):
-    if 'jac' not in string:
+    if 'jac' not in string.lower():
         raise ValueError("String does not contain jac")
     if '4' in string:
         res = JACUZZI4P

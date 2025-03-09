a349a6db35aa93801f7efa28a5a59b8a66740876
Sasha Keskin <skeskin@bwi.earth>
2025-03-09 19:54:55 +0100

    change initial data

--- dashboard/db/create/insert_initial_data.sql
+++ dashboard/db/create/insert_initial_data.sql
@@ -1,8 +1,8 @@
 INSERT INTO users (username, password, email, is_staff, country, zone, phone_number) VALUES
-('python', 'pbkdf2:sha256:1000000$SHQVsanyO7Nb07oQ$63a83e27956014ec3bcbab3bb28ef8afa6c30c79a8dffc77f413c969499b56b5', 'none@yopmail.com', true, 'France', 'ile_de_france', '+33666666666'),
-('python2', 'pbkdf2:sha256:1000000$SHQVsanyO7Nb07oQ$63a83e27956014ec3bcbab3bb28ef8afa6c30c79a8dffc77f413c969499b56b5', 'none2@yopmail.com', true, 'France', 'var', '+33666666666'),
-('python3', 'pbkdf2:sha256:1000000$SHQVsanyO7Nb07oQ$63a83e27956014ec3bcbab3bb28ef8afa6c30c79a8dffc77f413c969499b56b5', 'none3@yopmail.com', true, 'France', 'pays_de_gex', '+33666666666'),
-('python4', 'pbkdf2:sha256:1000000$SHQVsanyO7Nb07oQ$63a83e27956014ec3bcbab3bb28ef8afa6c30c79a8dffc77f413c969499b56b5', 'none4@yopmail.com', true, 'France', 'loire', '+33666666666');
+('python', 'pbkdf2:sha256:1000000$3DdeCIsJlSyGs1Bi$9743fe7068be6602cab76b03edfdab326bdf62fe89793e5832098801a13db4ca', 'neuneu@yopmail.com', true, 'France', 'ile_de_france', '+33666666666'),
+('python2', 'pbkdf2:sha256:1000000$3DdeCIsJlSyGs1Bi$9743fe7068be6602cab76b03edfdab326bdf62fe89793e5832098801a13db4ca', 'neuneu2@yopmail.com', true, 'France', 'var', '+33666666666'),
+('python3', 'pbkdf2:sha256:1000000$3DdeCIsJlSyGs1Bi$9743fe7068be6602cab76b03edfdab326bdf62fe89793e5832098801a13db4ca', 'neuneu3@yopmail.com', true, 'France', 'pays_de_gex', '+33666666666'),
+('python4', 'pbkdf2:sha256:1000000$3DdeCIsJlSyGs1Bi$9743fe7068be6602cab76b03edfdab326bdf62fe89793e5832098801a13db4ca', 'neuneu4@yopmail.com', true, 'France', 'loire', '+33666666666');
 
 
 INSERT INTO user_delivery_zones (user_id, zone_name, center_geog, radius_km) VALUES

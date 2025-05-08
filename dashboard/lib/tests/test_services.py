814371b45d83dccbaec68492956d8da178eb5954
Sasha Keskin <skeskin@bwi.earth>
2025-05-08 16:46:52 +0200

    continue testing

--- dashboard/lib/tests/test_services.py
+++ dashboard/lib/tests/test_services.py
@@ -1,26 +1,32 @@
 def test_check_delivery_men_around_point(test_db_client):
-    in_shipping_lat = 45.39
     result = test_db_client.call_rpc(
         "check_delivery_men_around_point",
-        {"in_shipping_lon": 4.31, "in_shipping_lat": in_shipping_lat},
+        {"in_shipping_lon": 4.31, "in_shipping_lat": 45.39},
     )
     assert result
 
-    in_shipping_lat = 91  # inexistant
     result = test_db_client.call_rpc(
         "check_delivery_men_around_point",
-        {"in_shipping_lon": 4.31, "in_shipping_lat": in_shipping_lat},
+        {"in_shipping_lon": 4.31, "in_shipping_lat": 91},  # fancy lat,
     )
     assert not result
 
 
 def test_check_availability_jacuzzi(test_db_client, client):
-    data = {
+    send_data = {
         "location": {"lat": 48.8566, "lon": 2.3522},
         "productName": "Jacuzzi 4 places 1 nuit",
     }
 
-    response = client.post("/services/check_availability", json=data)
+    response = client.post("/services/check_availability", json=send_data)
     assert response.status_code == 200
+    data = response.json
+    assert "rent_duration_day" in data
 
-    assert "rent_duration_day" in response.json
+    send_data = {
+        "location": {"lat": 48.8566, "lon": 181},  # fancy lon
+        "productName": "Jacuzzi 4 places 1 nuit",
+    }
+
+    response2 = client.post("/services/check_availability", json=send_data)
+    data2 = response.json

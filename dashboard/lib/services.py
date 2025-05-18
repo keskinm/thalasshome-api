770c2066ad7a6756212f1b594581ec323ab430ed
Sasha Keskin <skeskin@bwi.earth>
2025-05-18 13:06:25 +0200

    revert

--- dashboard/lib/services.py
+++ dashboard/lib/services.py
@@ -120,25 +120,7 @@ def create_20_draft():
                     f"Reste {round(full_price - deposit_price, 2)} € à payer à la livraison."
                 ),
                 "customer": {"email": customer_email},
-                "use_customer_default_address": False,
-                "shipping_address": {
-                    "first_name": "Prénom",  # obligatoire
-                    "last_name": "Nom",  # obligatoire
-                    "address1": "123 rue Exemple",
-                    "address2": "",  # optionnel
-                    "city": "Paris",  # obligatoire
-                    "province": "Île-de-France",
-                    "province_code": "IDF",
-                    "country": "France",  # ou ISO “FR”
-                    "country_code": "FR",
-                    "zip": "75000",  # obligatoire
-                    "phone": "+33 7 77 77 77 77",
-                },
-                "shipping_line": {
-                    "title": "Livraison standard",
-                    "price": "0.00",
-                    "code": "GRATUIT",
-                },
+                "use_customer_default_address": True,
             }
         }
 
